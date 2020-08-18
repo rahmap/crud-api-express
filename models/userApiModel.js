071947045219e6bc363d859a8de8224f056fe24c
Rahma TV <purwantiibuku@gmail.com>
2020-08-19 06:42:12 +0700

    add API create, read

--- models/userApiModel.js
+++ models/userApiModel.js
@@ -1,19 +1,7 @@
-const knex = require('../config/db_knex')
+let knex = require('../config/db_knex')
 
 module.exports = {
-  getAllUsers : function(){
-    return knex('users').then(results => results)
-  },
-
-  getSingleUser : function(id){
-    return knex('users').where('user_id', id).then(results => results)
-  },
-
-  addUser : function(data){
-    return knex('users').insert({
-      user_name : data.user_name,
-      user_address : data.user_address,
-      user_age : data.user_age
-    }).then(results => results[0]).catch(() => 0)
+  testConnection : function (){
+    console.log(knex)
   }
 }
--- routes/users_api.js
+++ routes/users_api.js
@@ -3,8 +3,5 @@ let router = express.Router();
 let userApi = require('../controllers/userApiController');
 
 router.get('/', userApi.index)
-router.get('/create', userApi.create)
-router.get('/:id', userApi.show)
-router.post('/create', userApi.store)
 
 module.exports = router;
